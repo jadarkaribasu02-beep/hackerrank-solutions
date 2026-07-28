fc3b9db31d1ac170405e3e44cbdef44e809596c8
Karibasu Jadar <jadarkaribasu02@gmail.com>
2026-07-28 19:47:01 +0530

    ✅ Java Strings Introduction [java] — approach1

---
+++ solutions/java/easy/java-strings-introduction/solution-approach1.java
@@ -0,0 +1,39 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-strings-introduction/problem?isFullScreen=true
+// Problem     Java Strings Introduction
+// Difficulty  Easy
+// Subdomain   Strings
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-07-28, 07:46 p.m.
+// ──────────────────────────────────────────────────
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        
+        
+        System.out.println(A.length() + B.length());
+        
+        
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+        
+        String capA = A.substring(0, 1).toUpperCase() + A.substring(1);
+        String capB = B.substring(0, 1).toUpperCase() + B.substring(1);
+        System.out.println(capA + " " + capB);
+        
+        sc.close();
+    }
+}
